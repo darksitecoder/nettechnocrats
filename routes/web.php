78b9854c2da8d85538c18ca24f481537c83e8085
Ashish <ashish.kumar@nettechnocrats.com>
2024-08-21 12:24:16 +0530

    get images

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DigitalMarketingController;
 use App\Http\Controllers\TechController;
+use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/', function () {
     return view('frontend.home');
@@ -11,3 +12,6 @@ Route::get('/', function () {
 Route::get('/digital-marketing', [DigitalMarketingController::class, 'digitalMarketing']);
 
 Route::get('/tech', [TechController::class, 'tech']);
+
+
+Route::get('/admin/dashboard', [DashboardController::class, 'dashboard']);
