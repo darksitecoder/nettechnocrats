196d29d791e58d91a1e50fe636cbe0fde0d93956
bhaskar <bhaskarpratap111@gmail.com>
2024-09-05 12:26:46 +0530

    dd

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DigitalMarketingController;
+use App\Http\Controllers\TechController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\IndustriesController;
+use App\Http\Controllers\PortfolioController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,3 +18,20 @@
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Route::get('/home', function () {
+//     return view('frontend.home');
+// });
+
+
+Route::get('/digital-marketing', [DigitalMarketingController::class, 'index']);
+Route::get('/tech', [TechController::class, 'index']);
+Route::get('/industries', [IndustriesController::class, 'index']);
+Route::get('/portfolio', [PortfolioController::class, 'index']);
