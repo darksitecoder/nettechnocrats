b1f2d7b40190f3b5826f2dd9501193cf4277f50a
darksitecoder <sumitpal4689@gmail.com>
2024-11-15 15:27:02 +0530

    ss

--- resources/views/admin/partials/left_sidebar.blade.php
+++ resources/views/admin/partials/left_sidebar.blade.php
@@ -1,52 +1,52 @@
 <nav id="sidebar" class="sidebar js-sidebar">
-			<div class="sidebar-content js-simplebar">
-				<a class="sidebar-brand" href="{{url('admin_dashboard')}}">
-          <span class="align-middle">NetTechnoCrats</span>
-        </a>
+	<div class="sidebar-content js-simplebar">
+		<a class="sidebar-brand" href="{{url('admin_dashboard')}}">
+			<span class="align-middle">NetTechnoCrats</span>
+		</a>
 
-				<ul class="sidebar-nav">
-					<li class="sidebar-header">
-						Pages
-					</li>
+		<ul class="sidebar-nav">
+			<li class="sidebar-header">
+				Pages
+			</li>
 
-					<li class="sidebar-item active">
-						<a class="sidebar-link" href="{{url('admin_dashboard')}}">
-              <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
-            </a>
-					</li>
+			<li class="sidebar-item dashboard">
+				<a class="sidebar-link" href="{{url('admin_dashboard')}}">
+					<i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+				</a>
+			</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="{{url('portfolio')}}">
-              <i class="align-middle" data-feather="user"></i> <span class="align-middle">PortFolio</span>
-            </a>
-					</li>
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="{{url('portfolio')}}">
+					<i class="align-middle" data-feather="user"></i> <span class="align-middle">PortFolio</span>
+				</a>
+			</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="{{url('listBlogsForAdmin')}}">
-              <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Blogs</span>
-            </a>
-					</li>
+			<li class="sidebar-item blog">
+				<a class="sidebar-link" href="{{url('listBlogsForAdmin')}}">
+					<i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Blogs</span>
+				</a>
+			</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages-sign-up.html">
-              <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Enquires</span>
-            </a>
-					</li>
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="pages-sign-up.html">
+					<i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Enquires</span>
+				</a>
+			</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages-blank.html">
-              <i class="align-middle" data-feather="book"></i> <span class="align-middle">Settings</span>
-            </a>
-					</li>
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="pages-blank.html">
+					<i class="align-middle" data-feather="book"></i> <span class="align-middle">Settings</span>
+				</a>
+			</li>
 
-					<!-- <li class="sidebar-header">
+			<!-- <li class="sidebar-header">
 						Tools & Components
 					</li>
 
 					<li class="sidebar-item">
 						<a class="sidebar-link" href="ui-buttons.html">
               <i class="align-middle" data-feather="square"></i> <span class="align-middle">Buttons</span> -->
-            <!-- </a>
+			<!-- </a>
 					</li>
 
 					<li class="sidebar-item">
@@ -87,19 +87,15 @@
 						<a class="sidebar-link" href="maps-google.html">
               <i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
             </a> -->
-					</li>
-				</ul>
+			</li>
+		</ul>
 
-				<div class="sidebar-cta">
-					<div class="sidebar-cta-content">
-						<strong class="d-inline-block mb-2">Go to Site</strong>
-						<div class="mb-3 text-sm">
-							Are you looking for more components? .
-						</div>
-						<div class="d-grid">
-							<a href="{{url('/')}}" class="btn btn-primary">Go to Site</a>
-						</div>
-					</div>
+		<div class="sidebar-cta">
+			<div class="sidebar-cta-content">
+				<div class="d-grid">
+					<a href="{{url('/')}}" class="btn btn-primary">Go to Site</a>
 				</div>
 			</div>
-		</nav>
+		</div>
+	</div>
+</nav>
