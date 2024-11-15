bea57f5962fc70a0314326e5f58bad685042ad1a
darksitecoder <sumitpal4689@gmail.com>
2024-11-15 16:10:18 +0530

    ss

--- resources/views/admin/pages/blogs/blogTopics.blade.php
+++ resources/views/admin/pages/blogs/blogTopics.blade.php
@@ -2,38 +2,69 @@
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Add Blog Topics</title>
+    <meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
+    <meta name="author" content="AdminKit">
+    <meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="shortcut icon" href="img/icons/icon-48x48.png" />
+
+    <link rel="canonical" href="https://demo-basic.adminkit.io/" />
+
+    <!-- Fonts -->
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
     <script src="https://kit.fontawesome.com/e1528f4468.js" crossorigin="anonymous"></script>
+    <!-- Fonts -->
+    <title>Add Topic</title>
 
-    <!-- Fonts and Icons -->
-    <link rel="icon" href="{{ asset('assets/frontEnd/web/images/favicon.ico') }}" type="image/x-icon" />
-    <link rel="shortcut icon" href="{{ asset('assets/frontEnd/web/images/favicon.ico') }}" type="image/x-icon" />
-    <link rel="stylesheet" href="{{ asset('/assets/select2/select2.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('theme/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('theme/assets/vendors/css/vendor.bundle.base.css') }}">
-    <link rel="stylesheet" href="{{ asset('theme/assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('style/admin-style/add-edit-style.css') }}">
-
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <!-- Load jQuery first -->
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-
-    <!-- Then load Bootstrap and other scripts -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-
+    <link href="{{ asset('/dashboard_theme/css/app.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
 
     <style>
-        * {
-            margin: 0;
-            padding: 0;
+        .edit {
+            transition: 0.5s ease-in-out;
+            background-color: #3B7DDD;
+            border: 1px solid #1CBB8C !important;
         }
+
+        .edit:hover {
+            background-color: transparent !important;
+            color: #1CBB8C;
+        }
+
+        .delete {
+            transition: 0.5s ease-in-out;
+            border: 1px solid #DC3545;
+        }
+
+        .delete:hover {
+            background-color: transparent !important;
+            color: #DC3545;
+        }
+
+        .btn {
+            padding: 8px 20px;
+            border: 1px solid #3B7DDD;
+            border-radius: 5px;
+            background-color: #3B7DDD;
+            color: #fff;
+            text-decoration: none;
+            transition: 0.5s ease-in-out;
+            font-weight: 600;
+        }
+
+        .btn:hover {
+            background-color: transparent;
+            color: #3B7DDD;
+        }
+
+
+
+        /*  */
+
 
         .feature-item {
             padding: 5px 10px;
@@ -77,45 +108,54 @@
 </head>
 
 <body>
-    <div class="container-scroller">
-        <div class="row p-0 m-0 proBanner" id="proBanner">
-            <div class="col-md-12 p-0 m-0"></div>
-        </div>
-    </div>
+    <div class="wrapper">
 
-    @include('admin_theme/Partial/navbar')
-    <div class="container-fluid page-body-wrapper" style="padding:0; margin:0;">
-        @include('admin_theme/Partial/sidebar')
+        @include('admin/partials/left_sidebar')
 
-        <div class="main-panel">
-            <div class="content-wrapper">
-                <div class="" style="padding-bottom:10px; margin-top:-1.5rem;">
-                    @include('admin_theme/Partial/admin_header')
-                    <button class="navbar-toggler" type="button" data-toggle="offcanvas">
-                        <span class="mdi mdi-menu"></span>
-                    </button>
-                </div>
+        <div class="main">
 
-                <div class="row pt-3">
-                    <div class="col-lg-12">
-                        <div id="input-container" class="d-flex justify-content-start">
-                            <input type="text" id="input-text" placeholder="Enter text...">
-                            <a id="add-button" class="add_new_btn mx-5">Add Topic</a>
+            @include('admin/partials/header')
+
+            <main class="content">
+                <div class="container-fluid p-0">
+                    <div class="d-flex justify-content-between mb-4">
+                        <h1 class="h3 mb-3"><strong>Blog List</strong></h1>
+                        <div class="buttons">
+                            <a href="{{url('/blogTopics')}}" class="mx-3 btn">Add Topic <i class="fa-solid fa-circle-plus"></i></a>
+                            <a href="{{url('/AddBlogsForAdmin')}}" class=" btn">Create Blog <i class="fa-solid fa-circle-plus"></i></a>
                         </div>
-                        <br>
-                        <span class="msg_err" id="allReadyExist" style="color:red; font-size:15px;"></span>
                     </div>
-                </div>
-                <div class="row pt-3">
-                    <div class="col-lg-12">
-                        <div id="element-container"></div>
+
+                    <div class="row pt-3">
+                        <div class="col-lg-12">
+                            <div id="input-container" class="d-flex justify-content-start">
+                                <input type="text" id="input-text" placeholder="Enter text...">
+                                <a id="add-button" class="add_new_btn mx-5">Add Topic</a>
+                            </div>
+                            <br>
+                            <span class="msg_err" id="allReadyExist" style="color:red; font-size:15px;"></span>
+                        </div>
                     </div>
+                    <div class="row pt-3">
+                        <div class="col-lg-12">
+                            <div id="element-container"></div>
+                        </div>
+                    </div>
+
                 </div>
-            </div>
+            </main>
+
+            @include('admin/partials/footer')
+
         </div>
     </div>
 
-    <!-- JavaScript -->
+    <script src="{{ asset('dashboard_theme/js/app.js') }}"></script>
+    <script>
+        const childElement = document.querySelector('.blog');
+        childElement.classList.add('active');
+    </script>
+
     <script>
         $(document).ready(function() {
             var baseUrl = window.location.origin;
@@ -252,10 +292,6 @@
             });
         });
     </script>
-
-    <!-- Bootstrap Scripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
 </body>
 
