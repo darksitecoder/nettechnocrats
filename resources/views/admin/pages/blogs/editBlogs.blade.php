fce40608f730fbb508532ec7987b31573dfec61a
Ashish <ashish.kumar@nettechnocrats.com>
2024-12-04 14:24:42 +0530

    Net - 04-12-2024

--- resources/views/admin/pages/blogs/editBlogs.blade.php
+++ resources/views/admin/pages/blogs/editBlogs.blade.php
@@ -256,12 +256,8 @@
               <h1 class="h3 mb-3"><strong>Add Blog</strong></h1>
 
               <div class="buttons">
-                <button class="mx-3 btn save" name="action" value="save" type="Submit">Update<i class="fa-solid fa-bookmark"></i></button>
-                @if($Blogs[0]->status == 'save')
+                <button class="mx-3 btn save" name="action" value="save" type="Submit">Save<i class="fa-solid fa-bookmark"></i></button>
                 <button class="btn published" name="action" value="publish" type="Submit">Published <i class="fa-solid fa-inbox"></i></button>
-                @endif
-
-
               </div>
             </div>
 
