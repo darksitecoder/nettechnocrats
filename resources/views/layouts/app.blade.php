3915bfc2fda35c91ff834ee54567fcb728f2bf0a
darksitecoder <sumitpal4689@gmail.com>
2024-12-04 16:16:25 +0530

    ss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -237,7 +237,7 @@
                 </div>
                 <!-- menu end here -->
                 <div class="header-item item-right">
-                    <a href="#" class="phone__number"><i class="fa-solid fa-phone"></i>+91-9711411002</a>
+                    <a target="_blank" href="https://api.whatsapp.com/send?phone=9711411002&amp;text=I checked the website, and I have a few questions to ask" title="WhatsApp" class="phone__number"><i class="fa-solid fa-phone"></i>+91-9711411002</a>
                     <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
                     <a href="{{ url('contact-us')}}"><span class="get__in__touch">Get In Touch</span></a>
                     <a href="{{ url('login_page')}}"><span class="get__in__touch">Login</span></a>
