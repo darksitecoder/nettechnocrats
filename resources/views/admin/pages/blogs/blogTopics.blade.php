daa9156d82b2868101a6ec13a436bd0e08a10d66
Ashish <ashish.kumar@nettechnocrats.com>
2024-12-05 11:32:14 +0530

    Net - 04-12-2024

--- resources/views/admin/pages/blogs/blogTopics.blade.php
+++ resources/views/admin/pages/blogs/blogTopics.blade.php
@@ -238,7 +238,7 @@
 
                     $.ajax({
                         type: 'POST',
-                        url: baseUrl + '/addBlogTopicApi',
+                        url: '/addBlogTopicApi',
                         headers: {
                             'X-CSRF-TOKEN': csrfToken // Add the CSRF token to the headers
                         },
