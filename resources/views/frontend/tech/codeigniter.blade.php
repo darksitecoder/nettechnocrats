8bec4179b096ee8f4c7db9ff6864fa89f5abff21
darksitecoder <sumitpal4689@gmail.com>
2024-12-17 11:07:15 +0530

    ss

--- resources/views/frontend/tech/codeigniter.blade.php
+++ resources/views/frontend/tech/codeigniter.blade.php
@@ -43,7 +43,7 @@
     </div>
     <div class="col-xl-5 wow fadeInRight" data-wow-delay="0.2s">
      <div class=" rounded position-relative overflow-hidden  d-flex justify-content-center">
-      <img src="{{ asset('assets/web/sharepoint.webp') }}" class="img-fluid rounded " alt="">
+      <img src="{{ asset('assets/web/slider_img1.webp') }}" class="img-fluid rounded w-100 image-with-shadow" alt="">
       <!-- <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script><lottie-player src="https://lottie.host/c7bfe87d-3ff6-4393-a188-2e75c4e054f6/K6w0F2mLOc.json" background="#fff" speed="1" style="width: 100%; height: 400px" loop autoplay direction="1" mode="normal"></lottie-player> -->
      </div>
     </div>
@@ -76,7 +76,7 @@
     </div>
     <div class="col-xl-6 wow fadeInRight" data-wow-delay="0.2s">
      <div class="rounded position-relative overflow-hidden">
-      <img src="{{ asset('assets/web/sharepoint1.webp') }}" class="img-fluid rounded w-100" alt="">
+      <img src="{{ asset('assets/web/codeigniter-development-services-transformed.webp') }}" class="img-fluid rounded w-100" alt="">
 
      </div>
     </div>
@@ -232,10 +232,10 @@
       </div>
      </div>
     </div>
-    <div class="col-lg-6 content d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
-     <div class="seo__animation__gif">
-      <div class="rounded position-relative overflow-hidden">
-       <img src="{{ asset('assets/web/SharePoint-migration.webp') }}" class="img-fluid rounded w-100" alt="">
+    <div class="col-lg-6 content d-flex align-items-center " data-aos="fade-up" data-aos-delay="200">
+     <div class="seo__animation__gifs">
+      <div class="rounded position-relative overflow-hidden border-bottom-2">
+       <img src="{{ asset('assets/web/codeigneter-banner.webp') }}" class="img-fluid rounded w-100 image-with-shadow" alt="">
 
       </div>
      </div>
