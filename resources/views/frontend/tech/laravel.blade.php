3c3b339fbd8ea643e9ca7c8a0b563a4b69e35641
darksitecoder <sumitpal4689@gmail.com>
2024-12-17 15:59:22 +0530

    ss

--- resources/views/frontend/tech/laravel.blade.php
+++ resources/views/frontend/tech/laravel.blade.php
@@ -31,19 +31,19 @@
 <body>
  @include('partial/header')
  <!-- Abvout Start -->
- <div class="container-fluid about hero__banner__tech py-5" style="background: url('{{ asset('assets/web/tech__background.png') }}'); background-repeat:no-repeat; background-size:cover;">
-  <div class="container py-5">
+ <div class="container-fluid about hero__banner__tech py-1" style="background: url('{{ asset('assets/web/tech__background.png') }}'); background-repeat:no-repeat; background-size:cover;">
+  <div class="container py-3">
    <div class="row g-5 align-items-center" data-aos="fade-up" data-aos-delay="100">
     <div class="col-xl-7 wow fadeInLeft">
      <div>
       <!-- <h4 class="text-primary1">About Us</h4> -->
-      <h1 class="display-5 mb-4 text-white">Laravel Web Application Development Company In India</h1>
+      <h1 class="display-5 mb-4 text-white fs-2">Laravel Web Application Development Company In India</h1>
       <p class="mb-4 text-white">Get Cost-effective and highly futuristic Laravel solutions to develop business growth</p>
      </div>
     </div>
     <div class="col-xl-5 wow fadeInRight" data-wow-delay="0.2s">
      <div class=" rounded position-relative overflow-hidden  d-flex justify-content-center">
-      <img src="{{ asset('assets/web/sharepoint.webp') }}" class="img-fluid rounded " alt="">
+      <img src="{{ asset('assets/web/laravel__hero.webp') }}" class="img-fluid rounded w-100 image-with-shadow" alt="">
       <!-- <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script><lottie-player src="https://lottie.host/c7bfe87d-3ff6-4393-a188-2e75c4e054f6/K6w0F2mLOc.json" background="#fff" speed="1" style="width: 100%; height: 400px" loop autoplay direction="1" mode="normal"></lottie-player> -->
      </div>
     </div>
