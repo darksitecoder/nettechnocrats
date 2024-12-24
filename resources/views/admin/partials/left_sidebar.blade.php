c5e28878db24cb78960f62e5a8eccc631abf7150
darksitecoder <sumitpal4689@gmail.com>
2024-12-24 12:22:34 +0530

    ss

--- resources/views/admin/partials/left_sidebar.blade.php
+++ resources/views/admin/partials/left_sidebar.blade.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
+<script src="https://kit.fontawesome.com/e1528f4468.js" crossorigin="anonymous"></script>
 <nav id="sidebar" class="sidebar js-sidebar">
 	<div class="sidebar-content js-simplebar">
 		<a class="sidebar-brand" href="{{url('admin_dashboard')}}">
@@ -16,8 +18,51 @@
 			</li>
 
 			<li class="sidebar-item portfolio">
-				<a class="sidebar-link" href="{{url('PortfolioForAdmin')}}">
+				<a class="sidebar-link" data-bs-toggle="dropdown" href="#">
 					<i class="align-middle" data-feather="user"></i> <span class="align-middle">PortFolio</span>
+
+				</a>
+			</li>
+			<li class="sidebar-item">
+
+				<a class="nav-link" data-bs-toggle="collapse" href="#b2b-general-pages" aria-expanded="false"
+
+					aria-controls="b2b-general-pages">
+
+					<span class="menu-title text-white"><i class="fa-solid fa-diagram-project ps-3 pe-2"></i>&nbsp; Portfolio &nbsp;<i class="fa-solid fa-chevron-right"></i></span>
+
+				</a>
+
+				<div class="collapse ms-4" id="b2b-general-pages">
+
+					<ul class="nav flex-column sub-menu">
+
+						<li class="nav-item b2b-shop-request-admin-page">
+							<a class="nav-link" href="" onclick="markVisited(this)"><i class="fa-solid fa-bullhorn me-2" style="color:#fff; padding:2px; margin-left:0px;"></i>
+								<span class="menu-title text-white">Marketing Portfolio</span>
+							</a>
+						</li>
+
+						<li class="nav-item b2b-shop-users">
+							<a class="nav-link" href="" onclick="markVisited(this)"><i class="fa-solid fa-code me-2" style="color:#fff; padding:2px; margin-left:0px;"></i>
+								<span class="menu-title text-white">Development PortFolio</span>
+							</a>
+						</li>
+
+
+					</ul>
+
+				</div>
+
+			</li>
+			<li class="sidebar-item portfolio">
+				<a class="sidebar-link" href="{{url('PortfolioForAdmin')}}">
+					<i class="align-middle" data-feather="user"></i> <span class="align-middle">Marketing Portfolio</span>
+				</a>
+			</li>
+			<li class="sidebar-item portfolio">
+				<a class="sidebar-link" href="{{url('PortfolioForAdmin')}}">
+					<i class="align-middle" data-feather="user"></i> <span class="align-middle">Development PortFolio</span>
 				</a>
 			</li>
 
@@ -43,56 +88,56 @@
 				<a class="sidebar-link" href="pages-blank.html">
 					<i class="align-middle" data-feather="book"></i> <span class="align-middle">Settings</span>
 				</a>
-			</li> -->
+			</li>
 
-			<!-- <li class="sidebar-header">
-						Tools & Components
-					</li>
+			<li class="sidebar-header">
+				Tools & Components
+			</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-buttons.html">
-              <i class="align-middle" data-feather="square"></i> <span class="align-middle">Buttons</span> -->
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="ui-buttons.html">
+					<i class="align-middle" data-feather="square"></i> <span class="align-middle">Buttons</span> -->
 			<!-- </a>
-					</li>
+			</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-forms.html">
-              <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Forms</span>
-            </a>
-					</li>
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="ui-forms.html">
+					<i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Forms</span>
+				</a>
+			</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-cards.html">
-              <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
-            </a>
-					</li>
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="ui-cards.html">
+					<i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
+				</a>
+			</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-typography.html">
-              <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Typography</span>
-            </a>
-					</li>
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="ui-typography.html">
+					<i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Typography</span>
+				</a>
+			</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="icons-feather.html">
-              <i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
-            </a>
-					</li>
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="icons-feather.html">
+					<i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
+				</a>
+			</li>
 
-					<li class="sidebar-header">
-						Plugins & Addons
-					</li>
+			<li class="sidebar-header">
+				Plugins & Addons
+			</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="charts-chartjs.html">
-              <i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Charts</span>
-            </a>
-					</li>
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="charts-chartjs.html">
+					<i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Charts</span>
+				</a>
+			</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="maps-google.html">
-              <i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
-            </a> -->
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="maps-google.html">
+					<i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
+				</a> -->
 			</li>
 		</ul>
 
