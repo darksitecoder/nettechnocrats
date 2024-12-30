44cd8782552298cc831133790bf6a2d47cc19ff3
darksitecoder <sumitpal4689@gmail.com>
2024-12-30 11:23:30 +0530

    ss

--- resources/views/frontend/industries/delivery.blade.php
+++ resources/views/frontend/industries/delivery.blade.php
@@ -59,7 +59,7 @@
                   <div class="row g-5 align-items-center" data-aos="fade-up" data-aos-delay="100">
                         <div class="col-xl-12 wow fadeInLeft">
                               <div class=" rounded d-flex justify-content-center">
-                                    <img src="{{ asset('assets/web/tech__background.png') }}" class="img-fluid rounded" alt="" style="width: 100%; height:300px; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
+                                    <img src="{{ asset('assets/web/food-delivery.webp') }}" class="img-fluid rounded" alt="" style="width: 100%; height:500px; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
                               </div>
                         </div>
                   </div>
@@ -626,9 +626,9 @@
                               </div>
                         </div>
                         <div class="col-lg-6 content d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
-                              <div class="seo__animation__gif">
+                              <div class="seo__animation__gifs">
                                     <div class="rounded-4 position-relative overflow-hidden">
-                                          <img src="{{ asset('assets/web/android-image.webp') }}" class="img-fluid w-100" alt="">
+                                          <img src="{{ asset('assets/web/food-delivery-nettechnocrats.png') }}" class="img-fluid w-100 image-with-shadow" alt="">
 
                                     </div>
                               </div>
