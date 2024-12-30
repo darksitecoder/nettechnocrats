5c67f76c88ea6b6e8377bad6bf5e1bae8a632eef
darksitecoder <sumitpal4689@gmail.com>
2024-12-30 15:11:19 +0530

    ss

---
+++ resources/views/frontend/portfolio/portfolio_seo_detail.blade.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>SEO portfolio details</title>
+ <!-- <link rel="stylesheet" href="style.css"> -->
+
+ <link rel="stylesheet" href="{{ asset('style/web/home.css') }}">
+ <link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+  integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer" />
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous"
+  referrerpolicy="no-referrer" />
+
+
+
+ <!-- Libraries Stylesheet -->
+ <link rel="stylesheet" href="{{ asset('style/web/lib/animate/animate.min.css') }}" />
+ <link rel="stylesheet" href="{{ asset('style/web/lib/lightbox/css/lightbox.min.css') }}">
+ <link rel="stylesheet" href="{{ asset('style/web/lib/owlcarousel/assets/owl.carousel.min.css') }}">
+
+ <style>
+  .service .service-item {
+   height: 300px;
+  }
+
+  .design-features .service-item {
+   height: 550px !important;
+  }
+ </style>
+</head>
+
+<body>
+ @include('partial/header')
+ <!-- Abvout Start -->
+ <div class="container-fluid about hero__banner__tech py-5" style="background: url('{{ asset('assets/web/tech__background.png') }}'); background-repeat:no-repeat; background-size:cover;">
+  <div class="container py-5">
+   <div class="row g-5 align-items-center" data-aos="fade-up" data-aos-delay="100">
+    <div class="col-xl-12 wow fadeInLeft">
+     <div>
+      <!-- <h4 class="text-primary1">About Us</h4> -->
+      <h1 class="display-5 mb-4 text-white text-center">MSK Ultrasound Injections Clinic’s SEO Case Study</h1>
+
+     </div>
+    </div>
+   </div>
+  </div>
+ </div>
+ <!-- <div class="container-fluid ">
+  <div class="container py-2">
+   <div class="row g-5 align-items-center" data-aos="fade-up" data-aos-delay="100">
+    <div class="col-xl-12 wow fadeInLeft">
+     <div class=" rounded d-flex justify-content-center">
+      <img src="{{ asset('assets/web/healthcare.webp') }}" class="img-fluid rounded" alt="" style="width: 100%; height:500px; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
+     </div>
+    </div>
+   </div>
+  </div>
+ </div> -->
+ <div class="container-fluid about py-5">
+  <div class="container py-5">
+
+   <div class=" mx-auto wow fadeInUp" data-aos="fade-up" data-aos-delay="100" style="">
+    <h2 class="display-5 mb-4">About</h2>
+   </div>
+   <div class="row g-5 align-items-center" data-aos="fade-up" data-aos-delay="100">
+    <div class="col-xl-12 wow fadeInLeft">
+     <div>
+      <!-- <h4 class="text-primary1">About Us</h4> -->
+      <p class="mb-4">The world of healthcare marketing is intricate. Historically, promotional efforts within this sector faced strict scrutiny. Today, while the regulations, particularly HIPAA compliance, remain stringent, there’s a bright side: Healthcare SEO Services.</p>
+      <p class="mb-4">Navigating the unique challenges of the healthcare realm requires specialized solutions. As a healthcare provider, your brand messaging must be accurate and trustworthy. Moreover, with the U.S.’s unique market structure, simply convincing patients isn’t enough. The decision-making process often involves third-party intermediaries.</p>
+      <p class="mb-4">That’s where Nettechnocrats, a premier Healthcare SEO Company, steps in. We bridge the gap between intricate healthcare systems and effective online visibility. Our specialized SEO solutions ensure you reach your target audience efficiently and ethically.</p>
+      <p class="mb-4">Connect with our experts today for a transformative online strategy!</p>
+
+     </div>
+
+    </div>
+   </div>
+  </div>
+ </div>
+ <!-- About End -->
+
+ <!-- Services Start -->
+ <div class="container-fluid service pb-5">
+  <div class="container pb-5">
+   <h2 class="">When We Started</h2>
+   <p class="pb-2">Medical SEO, uniquely tailored by experts, is a specific branch of search engine optimization crafted for websites within the medical domain. While the foundational principles of SEO remain, the approach for medical websites demands added precision.</p>
+   <p class="pb-5">The distinction is evident in how search engines, particularly Google, rank and evaluate medical websites. These platforms expect such sites to consistently exhibit expertise, authority, and trustworthiness in their content. Medical SEO Services from Nettechnocrats ensure that these elevated criteria are not just met, but exceeded.</p>
+
+  </div>
+ </div>
+ <!-- Services End -->
+
+ <!-- Services Start -->
+
+ <div class="container-fluid ">
+  <div class="container py-2">
+   <h2 class="">The Results</h2>
+   <div class="row g-5 align-items-center" data-aos="fade-up" data-aos-delay="100">
+    <div class="col-xl-12 wow fadeInLeft">
+     <div class=" rounded d-flex justify-content-center">
+      <img src="{{ asset('assets/web/healthcare.webp') }}" class="img-fluid rounded" alt="" style="width: 100%; height:500px; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
+     </div>
+    </div>
+   </div>
+  </div>
+ </div>
+ <div class="container-fluid service pb-5">
+  <div class="container pb-5">
+   <p class=" pb-2">Empowering Healthcare Entities to Connect with Patients Online</p>
+   <p class=" pb-3">Nettechnocrats stands as a distinguished Healthcare SEO Company, dedicated to propelling medical businesses to their zenith. Our specialized Healthcare SEO Services are uniquely designed to escalate your online presence and capture the attention of prospective patients.</p>
+
+  </div>
+ </div>
+ <!-- Services End -->
+ <div class="container-fluid ">
+  <div class="container py-2">
+   <h2 class="">The Results</h2>
+   <div class="row g-5 align-items-center" data-aos="fade-up" data-aos-delay="100">
+    <div class="col-xl-12 wow fadeInLeft">
+     <div class=" rounded d-flex justify-content-center">
+      <img src="{{ asset('assets/web/healthcare.webp') }}" class="img-fluid rounded" alt="" style="width: 100%; height:500px; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
+     </div>
+    </div>
+   </div>
+  </div>
+ </div>
+ <div class="container-fluid service pb-5">
+  <div class="container pb-5">
+   <p class=" pb-2">Empowering Healthcare Entities to Connect with Patients Online</p>
+   <p class=" pb-3">Nettechnocrats stands as a distinguished Healthcare SEO Company, dedicated to propelling medical businesses to their zenith. Our specialized Healthcare SEO Services are uniquely designed to escalate your online presence and capture the attention of prospective patients.</p>
+
+  </div>
+ </div>
+ <!-- Services End -->
+
+
+ <!--  -->
+
+ <section class="brand__images py-4">
+  <div class="container">
+   <h3 class="text-center">Count On Our Passion For Technology</h3>
+   <p class="text-center">Since 2011, we have been going that extra mile to create exceptional digital strategies for our clients and their customers. That said, we are awarded by top-tier research firms and featured in premium magazines and journals.</p>
+   <div class="img__row">
+    <div class="brands"><img src="{{ asset('assets/web/common-img/Coca-Cola.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/5-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/7-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/8-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/9-1.webp') }}" alt="" srcset=""></div>
+   </div>
+   <div class="img__row">
+    <div class="brands"><img src="{{ asset('assets/web/common-img/10-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/11-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/bjp-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/bubble.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/chemists-world.webp') }}" alt="" srcset=""></div>
+   </div>
+   <div class="img__row">
+    <div class="brands"><img src="{{ asset('assets/web/common-img/dbl-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/omni-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/Preapps.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/tolo-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/1-1.webp') }}" alt="" srcset=""></div>
+   </div>
+  </div>
+ </section>
+
+ <!-- Call To Action Section -->
+ <section id="call-to-action" class="call-to-action section accent-background">
+
+  <div class="container">
+   <div class="row justify-content-center" data-aos="zoom-in" data-aos-delay="100">
+    <div class="col-xl-10">
+     <div class="text-center">
+      <h3>Ready To Get Started?</h3>
+      <p>Stay ahead from Crowd. We have an expert team of application developers who are experienced and proficient. We have good experience in bespoke web and mobile app development and are committed to delivering the best of the class solutions.</p>
+      <a class="cta-btn" href="#">Contact Us</a>
+     </div>
+    </div>
+   </div>
+  </div>
+
+ </section><!-- /Call To Action Section -->
+
+
+ @include('partial/footer')
+</body>
+
+</html>
+
+<script src="{{ asset('style/web/lib/wow/wow.min.js') }}"></script>
+<script src="{{ asset('style/web/lib/easing/easing.min.js') }}"></script>
+<script src="{{ asset('style/web/lib/waypoints/waypoints.min.js') }}"></script>
+<script src="{{ asset('style/web/lib/counterup/counterup.min.js') }}"></script>
+<script src="{{ asset('style/web/lib/lightbox/js/lightbox.min.js') }}"></script>
+<script src="{{ asset('style/web/lib/owlcarousel/owl.carousel.min.js') }}"></script>
+
+<script src="{{ asset('js/main.js') }}"></script>
