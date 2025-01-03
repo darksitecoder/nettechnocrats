7a96e7dbead458bea25c980eeaaec1f5e43edee6
darksitecoder <sumitpal4689@gmail.com>
2025-01-03 12:04:18 +0530

    ss

--- resources/views/frontend/digital-marketing/seo-services-for-hotels-and-restaurants.blade.php
+++ resources/views/frontend/digital-marketing/seo-services-for-hotels-and-restaurants.blade.php
@@ -52,7 +52,7 @@
     </div>
     <div class="col-xl-5 wow fadeInRight" data-wow-delay="0.2s">
      <div class=" rounded position-relative overflow-hidden  d-flex justify-content-center">
-      <img src="{{ asset('assets/web/sharepoint.webp') }}" class="img-fluid rounded " alt="">
+      <img src="{{ asset('assets/web/png-transparent-hotel-3d-palazzo-isonometriche-architecture-perspective-city-urban-free-vector-vectors-thumbnail-removebg-preview.webp') }}" class="img-fluid rounded " alt="">
       <!-- <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script><lottie-player src="https://lottie.host/c7bfe87d-3ff6-4393-a188-2e75c4e054f6/K6w0F2mLOc.json" background="#fff" speed="1" style="width: 100%; height: 400px" loop autoplay direction="1" mode="normal"></lottie-player> -->
      </div>
     </div>
@@ -88,7 +88,7 @@
     </div>
     <div class="col-xl-6 wow fadeInRight" data-wow-delay="0.2s">
      <div class="rounded position-relative overflow-hidden">
-      <img src="{{ asset('assets/web/sharepoint1.webp') }}" class="img-fluid rounded w-100" alt="">
+      <img src="{{ asset('assets/web/png-transparent-hotel-3d-sea-beach-isometric-perspective-city-urban-free-vector-vectors-removebg-preview.webp') }}" class="img-fluid rounded w-100 image-with-shadow" alt="">
 
      </div>
     </div>
@@ -379,9 +379,9 @@
      </div>
     </div>
     <div class="col-lg-6 content d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
-     <div class="seo__animation__gif">
+     <div class="seo__animation__gifs">
       <div class="rounded position-relative overflow-hidden">
-       <img src="{{ asset('assets/web/SharePoint-migration.webp') }}" class="img-fluid rounded w-100" alt="">
+       <img src="{{ asset('assets/web/74784125-hotel-icon-flat-design-vector-illustration-vector-removebg-preview.webp') }}" class="img-fluid rounded w-100 image-with-shadow" alt="">
 
       </div>
      </div>
