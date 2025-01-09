9ced612be0c3301b2fb89c9e99190c4b7af0ebb2
darksitecoder <sumitpal4689@gmail.com>
2025-01-09 17:41:33 +0530

    ss

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1444,7 +1444,7 @@ Let's say we want the selected one to fill a 40% of the container; so we have a 
                     </div>
                 </div>
             </section>
-            <section class="sample__banner my-3 " style="background: url('{{ asset('assets/web/sample__banner.png') }}');   background-size: cover;  background-position: center;  background-repeat: no-repeat; padding:10rem 0rem !important;">
+            <section class="sample__banner my-3 " style="background: url('{{ asset('assets/web/people.png') }}');   background-size: cover;  background-position: center;  background-repeat: no-repeat; padding:5rem 0rem !important;">
                 <div class="row">
                     <div class="col-lg-12">
                         <h2>Consistently Effective, Cutting-Edge SEO Solutions for Higher Conversion Rates</h2>
