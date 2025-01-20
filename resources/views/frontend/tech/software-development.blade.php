dde0167b252f58eb9ebc03762d3173015def6bee
darksitecoder <sumitpal4689@gmail.com>
2025-01-20 16:00:19 +0530

    ss

--- resources/views/frontend/tech/software-development.blade.php
+++ resources/views/frontend/tech/software-development.blade.php
@@ -1,1 +1,309 @@
-<h1>software-development</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Software Development Services</title>
+ <!-- <link rel="stylesheet" href="style.css"> -->
+
+ <link rel="stylesheet" href="{{ asset('style/web/home.css') }}">
+ <link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+  integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer" />
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous"
+  referrerpolicy="no-referrer" />
+
+
+
+ <!-- Libraries Stylesheet -->
+ <link rel="stylesheet" href="{{ asset('style/web/lib/animate/animate.min.css') }}" />
+ <link rel="stylesheet" href="{{ asset('style/web/lib/lightbox/css/lightbox.min.css') }}">
+ <link rel="stylesheet" href="{{ asset('style/web/lib/owlcarousel/assets/owl.carousel.min.css') }}">
+
+ <style>
+  .service .service-item {
+   height: 320px;
+  }
+ </style>
+</head>
+
+<body>
+ @include('partial/header')
+ <!-- Abvout Start -->
+ <div class="container-fluid about hero__banner__tech py-5" style="background: url('{{ asset('assets/web/tech__background.png') }}'); background-repeat:no-repeat; background-size:cover;">
+  <div class="container py-5">
+   <div class="row g-5 align-items-center" data-aos="fade-up" data-aos-delay="100">
+    <div class="col-xl-7 wow fadeInLeft">
+     <div>
+      <!-- <h4 class="text-primary1">About Us</h4> -->
+      <h1 class="display-5 mb-4 text-white">Software Development Services</h1>
+      <p class="mb-4 text-white">Effective, scalable and robust software solutions to amaze your customers</p>
+     </div>
+    </div>
+    <div class="col-xl-5 wow fadeInRight" data-wow-delay="0.2s">
+     <div class=" rounded position-relative overflow-hidden  d-flex justify-content-center">
+      <img src="{{ asset('assets/web/432-4323860_software-development-vector-png-transparent-png-removebg-preview (1).png') }}" class="img-fluid rounded w-100 image-with-shadow" alt="">
+      <!-- <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script><lottie-player src="https://lottie.host/c7bfe87d-3ff6-4393-a188-2e75c4e054f6/K6w0F2mLOc.json" background="#fff" speed="1" style="width: 100%; height: 400px" loop autoplay direction="1" mode="normal"></lottie-player> -->
+     </div>
+    </div>
+   </div>
+  </div>
+ </div>
+ <!-- About End -->
+
+ <!-- Services Start -->
+ <div class="container-fluid service py-5">
+  <div class="container pb-5">
+   <!-- <h2 class="text-center">A Quick Glance of Top Industries We Serve</h2>
+   <p class="text-center pb-5">Nettechnocrats is a leading chatbot development company that assist their clients to build bots which will provide an unparalleled experience to their customers and automate their collaborations tasks.</p> -->
+   <div class="row g-4">
+    <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.2s">
+     <div class="service-item">
+      <div class="service-img"><i class="fa-brands fa-angular"></i></div>
+      <div class="rounded-bottom p-4">
+       <a href="#" class="h4 d-inline-block mb-4">Software Development</a>
+       <p class="mb-4">Innovative, well-designed and correctly implemented business software solutions that can maximize your company value.</p>
+       <!-- <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Learn More</a> -->
+      </div>
+     </div>
+    </div>
+    <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.4s">
+     <div class="service-item">
+      <div class="service-img">
+       <i class="fa-solid fa-cash-register"></i>
+      </div>
+      <div class="rounded-bottom p-4">
+       <a href="#" class="h4 d-inline-block mb-4">Business Application Development</a>
+       <p class="mb-4">Whether you are a start-up or a fully-fledged company, we are ready to assist you at every phase of development from conceptualization to development and support. </p>
+       <!-- <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Learn More</a> -->
+      </div>
+     </div>
+    </div>
+    <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.6s">
+     <div class="service-item">
+      <div class="service-img">
+       <i class="fa-solid fa-laptop-code"></i>
+      </div>
+      <div class="rounded-bottom p-4">
+       <a href="#" class="h4 d-inline-block mb-4">E-Commerce Application Development</a>
+       <p class="mb-4">Working in a cohesive manner with clients, we build scalable and secure E-commerce websites that turn your visitors into your customers. </p>
+       <!-- <a class="btn btn-primary rounded-pill py-2 px-4" href="#">Learn More</a> -->
+      </div>
+     </div>
+    </div>
+    <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.2s">
+     <div class="service-item">
+      <div class="service-img">
+       <i class="fa-solid fa-circle-nodes"></i>
+      </div>
+      <div class="rounded-bottom p-4">
+       <a href="#" class="h4 d-inline-block mb-4">Web Application Development</a>
+       <p class="mb-4">Our experts develops full-cycle SaaS solutions development, sites and portals for corporate projects as well as deployment and support services.</p>
+      </div>
+     </div>
+    </div>
+    <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.4s">
+     <div class="service-item">
+      <div class="service-img">
+       <i class="fa-brands fa-creative-commons-remix"></i>
+      </div>
+      <div class="rounded-bottom p-4">
+       <a href="#" class="h4 d-inline-block mb-4">Support & Maintenance</a>
+       <p class="mb-4">Whether you need software installation, or system upgrades and improvements, we will be your efficient resource to help you maximize ROI and improve your business processes.</p>
+      </div>
+     </div>
+    </div>
+    <!-- <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.6s">
+     <div class="service-item">
+      <div class="service-img">
+       <i class="fa-solid fa-microscope"></i>
+      </div>
+      <div class="rounded-bottom p-4">
+       <a href="#" class="h4 d-inline-block mb-4">MOBILE APP DEVELOPMENT USING ANGULAR JS</a>
+       <p class="mb-4">We are backed by a veteran team of Angular JS developers for Angular JS based products customization, extension, and integration.</p>
+      </div>
+     </div>
+    </div>
+    <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.6s">
+     <div class="service-item">
+      <div class="service-img">
+       <i class="fa-solid fa-cloud-arrow-up"></i>
+      </div>
+      <div class="rounded-bottom p-4">
+       <a href="#" class="h4 d-inline-block mb-4">MAINTENANCE & SUPPORT</a>
+       <p class="mb-4">Our expert consultants will guide your application development using best practices and rich expertise.</p>
+      </div>
+     </div>
+    </div>
+    <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.6s">
+     <div class="service-item">
+      <div class="service-img"><i class="fa-brands fa-nfc-directional"></i>
+      </div>
+      <div class="rounded-bottom p-4">
+       <a href="#" class="h4 d-inline-block mb-4">BEHAVIOR WITH DIRECTIVES</a>
+       <p class="mb-4">Angular provides extra functionality to the HTML with the use of directives. Have you ever wished that your browser would do new tricks for you? Well, now it can!</p>
+      </div>
+     </div>
+    </div> -->
+   </div>
+  </div>
+ </div>
+ <!-- Services End -->
+
+
+ <section class="light-background why__choose__Net">
+  <div class="container">
+   <h2>Why Nettechnocrats for Software Development Services?</h2>
+   <div class="row pt-2">
+    <div class="col-lg-6 position-relative align-self-start " data-aos="fade-up" data-aos-delay="100">
+     <div class="row mt-3">
+      <div class="col-lg-6">
+       <p class="why__answer" data-aos="fade-up" data-aos-delay="200">5+ years of average experience
+       </p>
+      </div>
+      <div class="col-lg-6">
+       <p class="why__answer" data-aos="fade-up" data-aos-delay="200">On time project delivery guaranteed
+       </p>
+      </div>
+     </div>
+     <div class="row">
+      <div class="col-lg-6">
+       <p class="why__answer" data-aos="fade-up" data-aos-delay="300">High sealed security
+       </p>
+      </div>
+      <div class="col-lg-6">
+       <p class="why__answer" data-aos="fade-up" data-aos-delay="300">Dedicated Project Managers
+       </p>
+      </div>
+     </div>
+     <div class="row">
+      <div class="col-lg-6">
+       <p class="why__answer" data-aos="fade-up" data-aos-delay="400">24*7 Support
+       </p>
+      </div>
+      <div class="col-lg-6">
+       <p class="why__answer" data-aos="fade-up" data-aos-delay="400">Hassle free communication
+       </p>
+      </div>
+     </div>
+     <div class="row">
+      <div class="col-lg-6">
+       <p class="why__answer" data-aos="fade-up" data-aos-delay="500">Maintenance Service
+       </p>
+      </div>
+      <div class="col-lg-6">
+       <p class="why__answer" data-aos="fade-up" data-aos-delay="500">User-Friendly Data Structure
+       </p>
+      </div>
+     </div>
+     <div class="row">
+      <div class="col-lg-6">
+       <p class="why__answer" data-aos="fade-up" data-aos-delay="600">5+ years of average experience
+       </p>
+      </div>
+      <div class="col-lg-6">
+       <p class="why__answer" data-aos="fade-up" data-aos-delay="600">On time project delivery guaranteed
+       </p>
+      </div>
+     </div>
+     <div class="row">
+      <div class="col-lg-6">
+       <p class="why__answer" data-aos="fade-up" data-aos-delay="700">Integrity & transparency
+       </p>
+      </div>
+      <div class="col-lg-6">
+       <p class="why__answer" data-aos="fade-up" data-aos-delay="700">Free no-obligation quote
+       </p>
+      </div>
+     </div>
+     <div class="row">
+      <div class="col-lg-6">
+       <p class="why__answer" data-aos="fade-up" data-aos-delay="800">Flexible Engagement Models
+       </p>
+      </div>
+      <div class="col-lg-6">
+       <p class="why__answer" data-aos="fade-up" data-aos-delay="800">Non-Discloser Agreement
+       </p>
+      </div>
+     </div>
+    </div>
+    <div class="col-lg-6 content d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
+     <div class="seo__animation__gifs">
+      <div class="rounded position-relative overflow-hidden">
+       <img src="{{ asset('assets/web/pngtree-software-development-and-programming-code-on-computer-vector-illustration-for-technology-picture-image_8444436.png') }}" class="img-fluid rounded w-100 image-with-shadow" alt="">
+
+      </div>
+     </div>
+    </div>
+   </div>
+  </div>
+ </section>
+
+ <!--  -->
+
+ <section class="brand__images py-4">
+  <div class="container">
+   <h3 class="text-center">Count On Our Passion For Technology</h3>
+   <p class="text-center">Since 2011, we have been going that extra mile to create exceptional digital strategies for our clients and their customers. That said, we are awarded by top-tier research firms and featured in premium magazines and journals.</p>
+   <div class="img__row">
+    <div class="brands"><img src="{{ asset('assets/web/common-img/Coca-Cola.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/5-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/7-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/8-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/9-1.webp') }}" alt="" srcset=""></div>
+   </div>
+   <div class="img__row">
+    <div class="brands"><img src="{{ asset('assets/web/common-img/10-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/11-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/bjp-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/bubble.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/chemists-world.webp') }}" alt="" srcset=""></div>
+   </div>
+   <div class="img__row">
+    <div class="brands"><img src="{{ asset('assets/web/common-img/dbl-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/omni-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/Preapps.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/tolo-1.webp') }}" alt="" srcset=""></div>
+    <div class="brands"><img src="{{ asset('assets/web/common-img/1-1.webp') }}" alt="" srcset=""></div>
+   </div>
+  </div>
+ </section>
+
+ <!-- Call To Action Section -->
+ <section id="call-to-action" class="call-to-action section accent-background">
+
+  <div class="container">
+   <div class="row justify-content-center" data-aos="zoom-in" data-aos-delay="100">
+    <div class="col-xl-10">
+     <div class="text-center">
+      <h3>Ready To Get Started?</h3>
+      <p>Stay ahead from Crowd. We have an expert team of application developers who are experienced and proficient. We have good experience in bespoke web and mobile app development and are committed to delivering the best of the class solutions.</p>
+      <a class="cta-btn" href="#">Contact Us</a>
+     </div>
+    </div>
+   </div>
+  </div>
+
+ </section><!-- /Call To Action Section -->
+
+
+ @include('partial/footer')
+</body>
+
+</html>
+
+<script src="{{ asset('style/web/lib/wow/wow.min.js') }}"></script>
+<script src="{{ asset('style/web/lib/easing/easing.min.js') }}"></script>
+<script src="{{ asset('style/web/lib/waypoints/waypoints.min.js') }}"></script>
+<script src="{{ asset('style/web/lib/counterup/counterup.min.js') }}"></script>
+<script src="{{ asset('style/web/lib/lightbox/js/lightbox.min.js') }}"></script>
+<script src="{{ asset('style/web/lib/owlcarousel/owl.carousel.min.js') }}"></script>
+
+<script src="{{ asset('js/main.js') }}"></script>
