a11a03cc584dd1387d6b59c6a2cc744d82768b40
darksitecoder <sumitpal4689@gmail.com>
2025-01-21 13:11:43 +0530

    ss

--- resources/views/partial/proposal.blade.php
+++ resources/views/partial/proposal.blade.php
@@ -6,39 +6,40 @@
 
 
 
-   <div class="col-lg-12">
-   @if(session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-@endif
+
+   @endif
    <form name="praposal_form" action="{{ route('proposal.store') }}" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="200">
     @csrf
     <div class="row gy-4">
 
-        <div class="col-md-6">
-            <input type="text" name="name" id="name" class="form-control" placeholder="Your Name" required>
-        </div>
+     <div class="col-md-6">
+      <input type="text" name="name" id="name" class="form-control" placeholder="Your Name" required>
+     </div>
 
-        <div class="col-md-6">
-            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-        </div>
+     <div class="col-md-6">
+      <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+     </div>
 
-        <div class="col-md-6">
-            <input type="text" class="form-control" name="subject" id="subject-field" placeholder="Request For...." required>
-        </div>
+     <div class="col-md-6">
+      <input type="text" class="form-control" name="subject" id="subject-field" placeholder="Request For...." required>
+     </div>
 
-        <div class="col-md-6 text-center">
-            <button type="submit">Send Me a Proposal <i class="fa-solid fa-arrow-right"></i></button>
-        </div>
+     <div class="col-md-6 text-center">
+      <button type="submit">Send Me a Proposal <i class="fa-solid fa-arrow-right"></i></button>
+     </div>
 
     </div>
-</form>
-
-   </div><!-- End Contact Form -->
-
-  </div>
+   </form>
+   <div class="col-lg-6">
+    @if(session('success'))
+    <div class="alert alert-success">
+     {{ session('success') }}
+    </div>
+   </div>
+  </div><!-- End Contact Form -->
 
  </div>
+
+</div>
 
 </div>
