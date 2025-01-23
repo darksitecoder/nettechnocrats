92fdedfccea15f59b295cef202e9b7b2b33585ee
darksitecoder <sumitpal4689@gmail.com>
2025-01-23 12:46:44 +0530

    ss

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -190,7 +190,7 @@
               </div>
             </li>
             <li class="menu-item-has-children">
-              <a href="{{url('/portfolio-seo-list')}}">Portfolio <i class="fa fa-angle-down"></i></a>
+              <a href="{{url('/portfolio-seo-list.php')}}">Portfolio <i class="fa fa-angle-down"></i></a>
               <div class="sub-menu mega-menu mega-menu-column-2">
                 <div class="list-item">
                   <h4 class="title fw-bold ps-4">Digital Marketing</h4>
