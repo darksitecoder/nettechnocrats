d46b29ca68712566ac0d43427a7b55137a774246
darksitecoder <sumitpal4689@gmail.com>
2025-01-23 15:57:41 +0530

    ss

--- resources/views/frontend/tech/angular.blade.php
+++ resources/views/frontend/tech/angular.blade.php
@@ -142,6 +142,7 @@
    height: 500px;
   } */
   </style>
+  @include('partial/google_analytics')
 </head>
 
 <body>
@@ -183,7 +184,7 @@
             <div class="row g-4">
 
               <div class="col-sm-6">
-                <a href="#" class="btn btn-primary rounded-pill py-3 px-5 flex-shrink-0">Contact Us</a>
+                <a href="{{ url('contact-us.php')}}" class="btn btn-primary rounded-pill py-3 px-5 flex-shrink-0">Contact Us</a>
               </div>
 
             </div>
@@ -558,7 +559,7 @@
           <div class="text-center">
             <h3>Ready To Get Started?</h3>
             <p>Stay ahead from Crowd. We have an expert team of application developers who are experienced and proficient. We have good experience in bespoke web and mobile app development and are committed to delivering the best of the class solutions.</p>
-            <a class="cta-btn" href="#">Contact Us</a>
+            <a class="cta-btn" href="{{ url('contact-us.php')}}">Contact Us</a>
           </div>
         </div>
       </div>
--- resources/views/frontend/tech/codeigniter.blade.php
+++ resources/views/frontend/tech/codeigniter.blade.php
@@ -136,6 +136,7 @@
  <link rel="stylesheet" href="{{ asset('style/web/lib/animate/animate.min.css') }}" />
  <link rel="stylesheet" href="{{ asset('style/web/lib/lightbox/css/lightbox.min.css') }}">
  <link rel="stylesheet" href="{{ asset('style/web/lib/owlcarousel/assets/owl.carousel.min.css') }}">
+ @include('partial/google_analytics')
 </head>
 
 <body>
@@ -149,44 +150,7 @@
       <!-- <h4 class="text-primary1">About Us</h4> -->
       <h1 class="display-5 mb-4 text-white">CodeIgniter Development</h1>
       <p class="mb-4 text-white">Highly-customized CodeIgniter development services to empower your business</p>
-     </div>
-     <div id="contact" class="contact__section">
-
-      <div class="container" data-aos="fade-up" data-aos-delay="100">
-
-       <div class="row gy-4">
-
-
-
-        <div class="col-lg-12">
-         <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="200">
-          <div class="row gy-4">
-
-           <div class="col-md-6">
-            <input type="text" name="name" id="name-field" class="form-control" placeholder="Your Name">
-           </div>
-
-           <div class="col-md-6">
-            <input type="email" class="form-control" name="email" id="email-field" placeholder="Your Email" required="">
-           </div>
-
-           <div class="col-md-6">
-            <input type="text" class="form-control" name="subject" id="subject-field" placeholder="Request For...." required="">
-           </div>
-
-           <div class="col-md-6 text-center">
-            <button type="submit">Send Me a Proposal <i class="fa-solid fa-arrow-right"></i>
-            </button>
-           </div>
-
-          </div>
-         </form>
-        </div><!-- End Contact Form -->
-
-       </div>
-
-      </div>
-
+      @include('partial/proposal')
      </div>
     </div>
     <div class="col-xl-5 wow fadeInRight" data-wow-delay="0.2s">
